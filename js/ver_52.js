c16cfabac6fb5d4f73daae9b4ef16f39ee04e979
Aryan Jaiswal <aryanjaiswal@Aryans-MacBook-Air-2.local>
2024-06-28 23:12:55 +0530

    Made JavaScript code in proper format and orientation.

--- js/ver_52.js
+++ js/ver_52.js
@@ -1,12 +1,10 @@
-;(function ($) {
+(function ($) {
+  "use strict";
 
-    "use strict";
-
-    $(document).ready(function () {
-        $('.pxl-date input').datetimepicker({
-            timepicker: true,
-            format:'H:i - F j, Y'
-        });  
+  $(document).ready(function () {
+    $(".pxl-date input").datetimepicker({
+      timepicker: true,
+      format: "H:i - F j, Y",
     });
-
+  });
 })(jQuery);
